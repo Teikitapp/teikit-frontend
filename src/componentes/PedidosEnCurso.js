ad67b018519abc59edd04faa99d56f0fbfe799a2
Teikit <teikit.app@gmail.com>
2025-02-08 11:50:49 -0300

    agregar usuario a log

--- src/componentes/PedidosEnCurso.js
+++ src/componentes/PedidosEnCurso.js
@@ -29,7 +29,7 @@ const PedidosEnCurso = ({
                     //console.log(responseBuscarEstado.data);
                     
                 }).catch(error => {
-                    console.log(error);
+                    console.log(error, usuario.id);
                 })
             }
         }, []); 
