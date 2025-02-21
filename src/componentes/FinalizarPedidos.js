2efe79bb7d3b34cab22de0287506a4bd28634ce0
Teikit <teikit.app@gmail.com>
2025-02-21 16:59:22 -0300

    modificacion horario

--- src/componentes/FinalizarPedidos.js
+++ src/componentes/FinalizarPedidos.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../estilos/FinalizarPedidos.css';
 import ScrollTop from './ScrollTop';
 import DatePicker, { registerLocale } from 'react-datepicker';
