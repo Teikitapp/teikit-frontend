95c20cb27dbe7dc97a219d057536d892594f1742
Teikit <teikit.app@gmail.com>
2025-03-15 19:54:42 -0300

    Update HomeComercio.js

--- src/componentes/cafeteria/HomeComercio.js
+++ src/componentes/cafeteria/HomeComercio.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import CardProductosComercio from '../cafeteria/CardProductosComercio';
 import logoCafeteria from "../../imagenes/logoCafeteria.png";
 import { useNavigate } from 'react-router';
