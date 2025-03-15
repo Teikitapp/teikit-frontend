2cc19d612c596d70da9323f1ade1ce7fbe492cd0
Teikit <teikit.app@gmail.com>
2025-03-15 19:46:38 -0300

    Update HomeComercio.js

--- src/componentes/cafeteria/HomeComercio.js
+++ src/componentes/cafeteria/HomeComercio.js
@@ -37,13 +37,6 @@ const HomeComercio = () => {
   useEffect(() => {
       ClienteService.obtenerPedidos(1).then(response => {
         console.log("RESPONSE: ", response);    
-        
-          if(response.data.length === 0){
-            setBool(true);
-          }else{
-            setBool(false);
-          }
-          setListaPedidos(response.data);
          
       }).catch(error => {
         console.log(error);
