6bb9678db735339d12494b6b05813dda60f54432
Teikit <teikit.app@gmail.com>
2025-03-16 11:29:11 -0300

    Update HomeComercio.js

--- src/componentes/cafeteria/HomeComercio.js
+++ src/componentes/cafeteria/HomeComercio.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router';
 import ClienteService from '../../servicios/ClienteService';
 import "../../estilos/HomeComercio.css";
 import * as XLSX from 'xlsx';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const HomeComercio = () => {
 
