b38b8aa22e8b9e659e4bebe74c4b85a2fb94d017
Teikit <teikit.app@gmail.com>
2025-03-16 10:14:04 -0300

    Update HomeComercio.js

--- src/componentes/cafeteria/HomeComercio.js
+++ src/componentes/cafeteria/HomeComercio.js
@@ -4,8 +4,8 @@ import logoCafeteria from "../../imagenes/logoCafeteria.png";
 import { useNavigate } from 'react-router';
 import ClienteService from '../../servicios/ClienteService';
 import "../../estilos/HomeComercio.css";
-import * as XLSX from 'xlsx';
-import { Link } from 'react-router-dom';
+//import * as XLSX from 'xlsx';
+//import { Link } from 'react-router-dom';
 
 const HomeComercio = () => {
 
@@ -53,105 +53,6 @@ const HomeComercio = () => {
   };
 
   /*INICIO*/
-state = {
-        woorksheets: [],
-        files: [],
-        propiedades: [],
-        status: false
-    }
-
-    selectHola = React.createRef();
-
-    leerrFilas = (index) =>{
-       let hoja = this.state.woorksheets[index];
-       let files = XLSX.utils.sheet_to_row_object_array(hoja.data);
-       this.state.files=[];
-       this.state.files = files;
-       
-       this.ingresarPedidosBD(files);
-     }
-
-     ingresarPedidosBD = (files) => {
-     let body = { 
-            "nombre" : null,
-            "precioUnidad" : null,
-            "categoria": null,
-            "descripcion": null,
-            "idCliente": null
-        }
-
-        for (let index = 0; index < files.length; index++) {
-            body = { 
-                "nombre" : files[index].PRODUCTO,
-                "precioUnidad" : files[index].PRECIO,
-                "categoria": null,
-                "descripcion": null,
-                "idCliente": 1
-            }
-
-            ClienteService.guardarProductos(body).then(response => {
-            }).catch(error => {
-                console.log(error);
-            })
-            
-        }      
-     }
- 
-     leerPropiedades = (index) => {
-         let hoja = this.state.woorksheets[index];
-         this.state.propiedades = [];
-         for (let key in hoja.data) {
-            let regEx = new RegExp("^\(\\w\)\(1\){1}$");
-            if (regEx.test(key) === true) {
-                this.state.propiedades.push(hoja.data[key].v)
-            }          
-         }       
-     }
-
-     cambiarHoja = () => {
-        this.leerPropiedades(this.selectHola.curretnt.value);
-        this.leerrFilas(this.selectHola.current.value);
-        this.setState({
-            files: this.state.files,
-            propiedades: this.state.propiedades
-        })
-     }
-
-    leerExcel = (e) => {
-
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        let excel = formData.get("excel");
-        let listWoorksheet = [];
-
-        let reader = new FileReader()
-        reader.readAsArrayBuffer(excel);
-        reader.onloadend = (e) => {
-            let data = new Uint8Array(e.target.result)
-            let excelRead = XLSX.read(data, { type: 'array' })
-            excelRead.SheetNames.forEach(function (SheetNames, index) {
-                listWoorksheet.push({
-                    data: excelRead.Sheets[SheetNames],
-                    name: SheetNames,
-                    index: index
-                })
-            })
-            this.state.woorksheets = listWoorksheet;
-            this.setState({
-                woorksheets: this.state.woorksheets
-            })
-
-            this.leerPropiedades(0);
-            this.leerrFilas(0);
-            this.setState({
-                files: this.state.files,
-                propiedades: this.state.propiedades,
-                status: true
-            })
-
-
-        }
-    }
 
 
   return (
