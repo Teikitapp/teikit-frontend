c3798286c361e237971f3c58d90da155c2ab1509
Teikit <teikit.app@gmail.com>
2025-04-22 05:03:43 +0000

    modificaion para landingPage

--- src/componentes/LandingPage.js
+++ src/componentes/LandingPage.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import landingPage from "../imagenes/pageLanding.png";
+import landingPage2 from "../imagenes/landingPage2.png";
+import landingPage3 from "../imagenes/landingPage3.png";
+import "../estilos/LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>hello!!</h1>
+    <div className='color-withe'>
+     
+      <div className="divUno" >
+        <img src={landingPage} alt='logo'></img>
+      </div>
+      <div className="divDos">
+        <img  src={landingPage2} alt='logo'></img>
+      </div>
+      <div className="divTres">
+        <img  src={landingPage3} alt='logo'></img>
+      </div>
     </div>
   )
 }
