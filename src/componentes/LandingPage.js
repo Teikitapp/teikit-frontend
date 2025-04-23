9bcb580ff69c47230c374e9f33323d9cd0c6c5f4
Teikit <teikit.app@gmail.com>
2025-04-23 01:28:38 +0000

    modificacion para response del landingPage

--- src/componentes/LandingPage.js
+++ src/componentes/LandingPage.js
@@ -3,19 +3,27 @@ import landingPage from "../imagenes/pageLanding.png";
 import landingPage2 from "../imagenes/landingPage2.png";
 import landingPage3 from "../imagenes/landingPage3.png";
 import "../estilos/LandingPage.css";
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
     <div className='color-withe'>
+    
+      <div  className=''>
+        <h1 className='texto'>Compra tu comida online y retira en casilleros.</h1>
+        <p className='texto'>¡Sin espera, en el horario que quieras!</p>
+      </div>
      
-      <div className="divUno" >
-        <img src={landingPage} alt='logo'></img>
+      <div className="divUno" >       
+        <Link to="/login"><img className='imagen' src={landingPage} alt='logo'></img></Link>
       </div>
-      <div className="divDos">
-        <img  src={landingPage2} alt='logo'></img>
+
+      <div className="divDos">       
+        <Link to="/login"><img className='imagen imagenDos' src={landingPage2} alt='logo'></img></Link>
       </div>
-      <div className="divTres">
-        <img  src={landingPage3} alt='logo'></img>
+
+      <div className="divTres">       
+        <Link to="/login"><img className='imagen imagenTres' src={landingPage3} alt='logo'></img></Link>
       </div>
     </div>
   )
